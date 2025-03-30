f38ed1725a492d3e992abcccec0f2b5086ce4172
VidarDev <86915969+VidarDev@users.noreply.github.com>
2025-03-30 12:13:49 +0200

    build(app): Architecture + dependances frontend
    Installation des dépendances frontend + Nouvelle architecture de projet

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,53 +1,57 @@
-"use client"
-import {useState} from "react";
-import {authClient} from "@/lib/auth-client";
-import {redirect} from "next/navigation";
+"use client";
+import { authClient } from "@/shared/lib/auth-client";
+import { redirect } from "next/navigation";
+import { useState } from "react";
 
 export default function SignIn() {
-    const [email, setEmail] = useState("")
-    const [error, setError] = useState<undefined | string>()
-    const [success, setSuccess] = useState(false)
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState<undefined | string>();
+  const [success, setSuccess] = useState(false);
 
-    const {data: session} = authClient.useSession()
+  const { data: session } = authClient.useSession();
 
-    if (session) {
-        redirect("/")
+  if (session) {
+    redirect("/");
+  }
+
+  async function sendMagicLink() {
+    const { data, error } = await authClient.signIn.magicLink({
+      email,
+      callbackURL: "/",
+    });
+
+    if (error) {
+      setError(error.message);
+    } else if (data) {
+      setSuccess(true);
+      setError(undefined);
     }
+  }
 
-    async function sendMagicLink() {
-        const {data, error} = await authClient.signIn.magicLink({
-            email,
-            callbackURL: "/"
-        })
-
-        if (error) {
-            setError(error.message)
-        } else if (data) {
-            setSuccess(true)
-            setError(undefined)
-        }
-    }
-
-    return (
+  return (
+    <div>
+      {!error && !success && (
         <div>
-            {!error && !success && (
-                <div>
-                    <input type="email" name="email" onChange={(e) => setEmail(e.target.value)}/>
-                    <button onClick={() => sendMagicLink()}>Envoyer le magic link</button>
-                </div>
-            )}
-            {success && (
-                <div>
-                    <p>Check your email for the magic link.</p>
-                </div>
-            )}
-
-            {error && (
-                <div>
-                    <p>Error: {error}</p>
-                    <button onClick={() => setError(undefined)}>Try again</button>
-                </div>
-            )}
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={() => sendMagicLink()}>Envoyer le magic link</button>
         </div>
-    )
+      )}
+      {success && (
+        <div>
+          <p>Check your email for the magic link.</p>
+        </div>
+      )}
+
+      {error && (
+        <div>
+          <p>Error: {error}</p>
+          <button onClick={() => setError(undefined)}>Try again</button>
+        </div>
+      )}
+    </div>
+  );
 }
