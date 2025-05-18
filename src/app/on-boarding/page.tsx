c8d1b5bebad9427efcffc66026ad210d76e5ebbd
Quentin <qhonnart@amphibee.fr>
2025-05-18 17:45:25 +0200

    add new fields in on boarding for profile page

--- src/app/on-boarding/page.tsx
+++ src/app/on-boarding/page.tsx
@@ -17,10 +17,7 @@ export default async function OnBoarding() {
             <div
                 className="hidden md:block md:w-1/3 relative bg-primary"
             >
-
-                <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-
-                <div className="relative h-full z-10 flex flex-col justify-center p-8 text-white">
+                <div className="fixed left-8 top-0 h-full z-10 flex flex-col justify-center p-8 text-white">
                     <h1 className="text-7xl font-bold mb-6">Artilink</h1>
                     <p className="mb-4 text-4xl">
                         Façonnez votre identité,<br/>
@@ -30,9 +27,6 @@ export default async function OnBoarding() {
             </div>
 
             <div className="w-full md:w-2/3 p-6 flex flex-col items-center justify-center">
-                <div className="w-full max-w-2xl mb-6">
-                    <h2 className="text-lg font-medium text-muted-foreground">Bienvenue, {session.user.email}</h2>
-                </div>
                 <OnBoardingForm user={session.user} />
             </div>
         </div>
